cc00b41099010a14c27f32236bf684896a295718
Adilet1603 <adiletakamtov641@gmail.com>
2025-05-18 19:08:25 +0600

    dds

--- mysite/settings.py
+++ mysite/settings.py
@@ -166,7 +166,7 @@ MODELTRANSLATION_LANGUAGE = ('en', 'ru')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticFILES')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 
